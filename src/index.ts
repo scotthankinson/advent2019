3f21a42247e59d21dfd89a7ad9fbbb7769bdaf6e
Scott Hankinson <scott.hankinson@slalom.com>
2019-12-20 10:43:31 -0500

    Progress

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
 import fs = require('fs');
 
 const start = (): void => {
-    //console.log(solve_dec_16_pt1());
-    console.log(solve_dec_16_pt2());
+    console.log(solve_dec_20_pt1());
+    // console.log(solve_dec_20_pt2());
 };
 
 module.exports = {
@@ -12,18 +12,23 @@ module.exports = {
 };
 
 
-const solve_dec_16_pt1 = () => {
+const solve_dec_20_pt1 = () => {
     try {
-        let data = fs.readFileSync('src/test.dec16.txt', 'utf8');
-        const signal = data.split('').map(numStr => parseInt(numStr));
+        let data = fs.readFileSync('src/test.dec20.txt', 'utf8');
         const lines = data.split('\n');
-
-        let lastSignal = JSON.parse(JSON.stringify(signal));
-        for (let i = 0; i < 100; i++){
-            lastSignal = crunchPhase(lastSignal);
-            console.log(i + ': ' + lastSignal.join(''));
+        let mapCoords = {};
+        for(let y = 0; y < lines.length; y++){
+            for (let x = 0; x < lines[y].length; x++){
+                let checkChar = lines[y].charAt(x);
+                if (checkChar === ' ' || checkChar === '#' || checkChar === '.' )
+                    continue;   
+                if (mapCoords[checkChar])
+                    mapCoords[checkChar].positions.push(x + ',' + y);
+                else
+                    mapCoords[checkChar] = { 'positions' : [x + ',' + y]};
+            }
         }
-        
+        console.log(mapCoords);
 
         return 0;
     } catch (e) {
@@ -32,63 +37,11 @@ const solve_dec_16_pt1 = () => {
     return -1;
 }
 
-const crunchPhase = (signal) =>{
-    let outPattern = [];
 
-    for(let i = 0; i < signal.length; i++) {
-        // console.log("Crunching digit " + i);
-        let phasePattern = [];
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(-1);
-        }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(0);
-        }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(1);
-        }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(0);
-        }
-        // console.log(phasePattern);
-
-        let localSignal = JSON.parse(JSON.stringify(signal));
-        let localRow = 0;
-        for(let j = 0; j < signal.length; j++){
-            // console.log(localSignal[j] + " times " + phasePattern[(j+1)%phasePattern.length]);
-            // console.log("Add " + (localSignal[j] * phasePattern[(j+1)%phasePattern.length]));
-            localRow += localSignal[j] * phasePattern[(j+1)%phasePattern.length];
-        }
-        // console.log("Tens digit: " + Math.abs(localRow % 10));
-        outPattern.push(Math.abs(localRow % 10));
-    }
-    return outPattern;
-}
-
-const solve_dec_16_pt2 = () => {
+const solve_dec_20_pt2 = () => {
     try {
-        let data = fs.readFileSync('src/test.dec16.txt', 'utf8');
-        let superData = "";
-        for(let i = 0; i < 34; i++){
-            superData += data;
-        }
-        const signal = superData.split('').map(numStr => parseInt(numStr));
-
-        let lastSignal = JSON.parse(JSON.stringify(signal));
-        for (let i = 0; i < 100; i++){
-            console.log("Processing Phase " + i);
-            lastSignal = crunchPhase(lastSignal);
-            // console.log(lastSignal.join(''));
-            // console.log(i + ': ' + lastSignal.join(''));
-        }
-        console.log(lastSignal.join(''));
-        /*
-        let offset = parseInt(lastSignal.join('').substring(0,7));
-        console.log("Offset: " + offset);
-        for(let i = offset; i < offset + 8; i++){
-            console.log(lastSignal[i]);
-        }
-        */
+        let data = fs.readFileSync('src/test.dec20.txt', 'utf8');
+        const lines = data.split('\n');
 
         return 0;
     } catch (e) {
--- src/solved/index.dec16.ts
+++ src/solved/index.dec16.ts
@@ -33,71 +33,100 @@ const solve_dec_16_pt1 = () => {
 }
 
 const crunchPhase = (signal) =>{
-    let outPattern = [];
-
+    let outPattern = new Array<number>(signal.length)
+    let localSignal = JSON.parse(JSON.stringify(signal));
+    // 37325517 was Too Low -- Save and Continue is borked
+    // 93242475 was Too High -- Troubleshoot!
+    // 49476260
     for(let i = 0; i < signal.length; i++) {
-        // console.log("Crunching digit " + i);
-        let phasePattern = [];
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(-1);
+        // Can we ignore the first half? We only ever look at the back side
+        if (i < signal.length / 2){
+            outPattern[i] = 0;
+            continue;    
         }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(0);
-        }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(1);
-        }
-        for(let j = 0; j < i+1; j++){
-            phasePattern.unshift(0);
-        }
-        // console.log(phasePattern);
-
-        let localSignal = JSON.parse(JSON.stringify(signal));
+        if (i % 10000 == 0) console.log("Row " + i + " / " + signal.length);
         let localRow = 0;
-        for(let j = 0; j < signal.length; j++){
-            // console.log(localSignal[j] + " times " + phasePattern[(j+1)%phasePattern.length]);
-            // console.log("Add " + (localSignal[j] * phasePattern[(j+1)%phasePattern.length]));
-            localRow += localSignal[j] * phasePattern[(j+1)%phasePattern.length];
+        
+        if (i * 4 > signal.length) {
+            let add = getRangeForSign(i, 1);
+            let sub = getRangeForSign(i, -1);
+            if (add[1] > signal.length - 1) add[1] = signal.length - 1;
+            for(let j = add[0]; j <= add[1]; j++){
+                localRow += localSignal[j];
+            }
+            if (sub[0] <= signal.length){
+                if (sub[1] > signal.length - 1) sub[1] = signal.length - 1;
+                for(let j = sub[0]; j <= sub[1]; j++){
+                    localRow -= localSignal[j];
+                }
+            }
+        } else {    
+            for(let j = 0; j < signal.length; j++){
+                localRow += localSignal[j] * getPhasePattern(i, j);
+            }
         }
-        // console.log("Tens digit: " + Math.abs(localRow % 10));
-        outPattern.push(Math.abs(localRow % 10));
+        
+        outPattern[i] = Math.abs(localRow % 10);
     }
     return outPattern;
+}
+
+// Calculate range at each iteration for 1,1,1,1 and -1,-1,-1,-1; try to cut out 0's
+const getRangeForSign = (iteration, sign) => {
+    // Iteration = 0:  [1, 0, -1, 0]    1 Start: 0, End: 0       -1 Start: 2, End: 2     
+    // Iterations = 1: [0, 1, 1, 0, 0, -1, -1, 0]   1 Start: 1, End: 2      -1 Start: 5, End: 6
+    // Iteration  = 2: [0, 0,  1, 1, 1, 0, 0, 0, -1, -1, -1, 0]     1 Start: 2, End: 4      -1 Start 8, End 10
+    if (sign === 1){
+        return [iteration, 2 * iteration];
+    }
+    if (sign === -1){
+        return [(iteration * 3) + 2, (iteration * 3) + 2 + iteration];
+    }
+}
+
+const getPhasePattern = (iteration, position) => {
+    let n = iteration + 1;
+    let phaseLength = n * 4;
+    let shiftPosition = (position + 1) % phaseLength;
+    if (shiftPosition < n) return 0;   
+    if (shiftPosition >= n && shiftPosition < (2*n)) return 1;
+    if (shiftPosition >= (2*n) && shiftPosition < (n * 3)) return 0;
+    return -1;
 }
 
 const solve_dec_16_pt2 = () => {
     try {
         let data = fs.readFileSync('src/test.dec16.txt', 'utf8');
         let superData = "";
-        for(let i = 0; i < 34; i++){
+        let iterations = 1656;
+        for(let i = 0; i < iterations; i++){ 
             superData += data;
         }
+
         const signal = superData.split('').map(numStr => parseInt(numStr));
 
         let lastSignal = JSON.parse(JSON.stringify(signal));
         for (let i = 0; i < 100; i++){
             console.log("Processing Phase " + i);
             lastSignal = crunchPhase(lastSignal);
-            // console.log(lastSignal.join(''));
-            // console.log(i + ': ' + lastSignal.join(''));
         }
+        fs.appendFileSync('scratch.day16.final.txt', lastSignal.join(''));
         console.log(lastSignal.join(''));
-        /*
-        let offset = parseInt(lastSignal.join('').substring(0,7));
-        console.log("Offset: " + offset);
-        for(let i = offset; i < offset + 8; i++){
+        
+        // lastSignal = testSolution.split('');
+        let offset = (10000 * data.length) - parseInt(data.substring(0,7));
+        let startOffset= (superData.length) - offset;
+        
+        for(let i = startOffset; i < startOffset + 8; i++){
             console.log(lastSignal[i]);
         }
-        */
 
-        return 0;
+        return "@";
     } catch (e) {
         console.log('Error:', e.stack);
     }
     return -1;
 }
-
-
 
 start();
 
